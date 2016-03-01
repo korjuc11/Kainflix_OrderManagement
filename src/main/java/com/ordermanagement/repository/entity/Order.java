6ba5b42402c71160120beb6c84969a770c8f9a43
korju11 <korjuc11@htlkaindorf.at>
2016-03-01 13:07:58 +0100

    entity

--- src/main/java/com/ordermanagement/repository/entity/Order.java
+++ src/main/java/com/ordermanagement/repository/entity/Order.java
@@ -1,7 +1,15 @@
 package com.ordermanagement.repository.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Julian on 01.03.2016.
  */
+@Entity
+@Table
 public class Order {
+
+    private int OrderID;
+    private
 }
