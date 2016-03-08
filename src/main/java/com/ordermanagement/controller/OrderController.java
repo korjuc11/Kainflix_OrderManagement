2ea1e626c9c674b9bf1785ee697408348a0e9724
korju11 <korjuc11@htlkaindorf.at>
2016-03-08 12:17:11 +0100

    order and productorder edited

--- src/main/java/com/ordermanagement/controller/OrderController.java
+++ src/main/java/com/ordermanagement/controller/OrderController.java
@@ -5,10 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.annotation.HttpMethodConstraint;
--- src/main/java/com/ordermanagement/repository/entity/Order.java
+++ src/main/java/com/ordermanagement/repository/entity/Order.java
@@ -1,15 +1,43 @@
 package com.ordermanagement.repository.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 /**
  * Created by Julian on 01.03.2016.
  */
 @Entity
 @Table
-public class Order {
+public class Order implements Serializable {
 
-    private int OrderID;
+    @GeneratedValue
+    @Id
+    @Column
+    private int orderID;
+
+    @Column
+    private String description;
+
+
+
+    public int getId()
+    {
+        return this.orderID;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public void setId(int id)
+    {
+        this.orderID =id;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
 
 }
